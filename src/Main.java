41ac2b7dc3d5c5239b9539902d2b69cca6957a5d
munir45 <memamy3@uw.edu>
2024-04-27 23:10:09 -0700

    add table

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
-import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        String create = "CREATE TABLE Persons (\n" +
-                "    PersonID int,\n" +
-                "    LastName varchar(255),\n" +
-                "    FirstName varchar(255),\n" +
-                "    Address varchar(255),\n" +
-                "    City varchar(255)\n" +
-                ");";
 
-        String add = "INSERT INTO Persons VALUES (12, 'Emam', 'Munir', '12341', 'Seattle')";
 
         String url = "jdbc:postgresql://localhost:5432/javadatabase";
         String username = "postgres";
         String password = "4545";
-        Connection con = DriverManager.getConnection(url, username, password);
-        Statement st = con.createStatement();
-       // ResultSet rs = st.executeQuery(create);
-        st.executeQuery(add);
-        con.commit();
-//        System.out.println(rs);
+
+        Manipulate test = new Manipulate(url, username, password);
+        HashMap<String, String> colNames = new HashMap<>();
+        colNames.put("name", "varchar(255)");
+        test.createTable("Test",colNames);
     }
 }
